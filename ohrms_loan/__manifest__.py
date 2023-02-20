ad38e6d2be2199b675facf7cbaf4b3e2f4e9f860
Ernad Husremovic <hernad@bring.out.ba>
2023-02-20 09:44:50 +0100

    fuelboss dependence OUT

--- ohrms_loan/__manifest__.py
+++ ohrms_loan/__manifest__.py
@@ -33,7 +33,7 @@
     'live_test_url': 'https://youtu.be/lAT5cqVZTZI',
     'website': "https://www.openhrms.com",
     'depends': [
-        'base', 'hr', 'payroll', 'l10n_bs_hr_payroll_fuelboss',
+        'base', 'hr', 'payroll'
     ],
     'data': [
         'security/ir.model.access.csv',
